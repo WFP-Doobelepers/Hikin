56fc99923e463ebdc1ecb0ecd47f685b7c1c69db
Xyncross1111 <anasmk2003@gmail.com>
2023-03-29 20:55:02 +0530

    update: reloadCommands to add logging

--- src/commands/reloadCommandsCommand.ts
+++ src/commands/reloadCommandsCommand.ts
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { RESTPostAPIApplicationCommandsJSONBody } from 'discord-api-types'
-import { CommandInteraction, GuildMember } from 'discord.js'
+import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js'
 import { discordBot } from '..'
 import { isBotAdmin } from '../utils'
 import { Command } from './command'
@@ -15,16 +15,30 @@ export default class ReloadCommandsCommand implements Command {
 
     async execute(interaction: CommandInteraction): Promise<void> {
 
+        interaction.deferReply();
+
         const User = interaction.member as GuildMember
 
-        if (!isBotAdmin(interaction.member as GuildMember) /* && (interaction.guildId != '1084456230833102968') */ && User.roles.cache.has('1084778349542518805') ) {
-            await interaction.reply({content: 'Only Bot Admins may use this command', ephemeral: true})
+        if (!isBotAdmin(interaction.member as GuildMember) /* && (interaction.guildId != '1084456230833102968') */ && User.roles.cache.has('1084778349542518805')) {
+            await interaction.editReply('**ERROR:** Missing Permissions.')
             return
         }
-        
-        await interaction.deferReply({ephemeral: true})
-        await discordBot.loadCommands()
-        await interaction.editReply('Reloaded commands!')
-    }
 
+        await discordBot.loadCommands()
+        console.log(`${new Date().toTimeString} Reloaded Commands by ${interaction.user.username} in ${interaction.guild?.name}`)
+
+        const embed =  new MessageEmbed()
+                            .setTitle('Reloaded Commands')
+                            .setDescription(`Reloadeded commands at ${new Date().toDateString}`)
+                            .setColor('ORANGE')
+                            .setThumbnail(<string> interaction.guild?.iconURL())
+                            .setFooter({
+                                text: `Requested by ${interaction.user.username}`,
+                                iconURL: `${interaction.user.avatarURL()}`
+                            })
+                            .setTimestamp(Date.now())
+                            .toJSON()
+
+        await interaction.editReply({embeds: [embed]})
+    }
 }
